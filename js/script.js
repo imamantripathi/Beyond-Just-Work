5c02c6a1defe9df03234bcee80eea696e55253fd
Aman Tripathi <imamantripathi@gmail.com>
2023-04-02 13:08:21 +0530

    Setup

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// grab everything we need
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+// add event listeners
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+// Hero Section 
+const slidesContainer = document.querySelector(".slides-container");
+const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+
+nextButton.addEventListener("click", () => {
+	slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+	slidesContainer.scrollLeft -= slideWidth;
+});
