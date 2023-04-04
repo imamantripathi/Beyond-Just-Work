b4ed77e0c78aec82fcb73e9372b2e169aed59343
Aman Tripathi <imamantripathi@gmail.com>
2023-04-04 21:29:45 +0530

    Home Page complete

--- js/script.js
+++ js/script.js
@@ -7,16 +7,17 @@ btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
 
-// Hero Section 
+// Hero Section
 const slidesContainer = document.querySelector(".slides-container");
 const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
 const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
 nextButton.addEventListener("click", () => {
-	slidesContainer.scrollLeft += slideWidth;
+  slidesContainer.scrollLeft += slideWidth;
 });
 
 prevButton.addEventListener("click", () => {
-	slidesContainer.scrollLeft -= slideWidth;
+  slidesContainer.scrollLeft -= slideWidth;
 });
+
