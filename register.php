f3479669932a3398342baefecc4d290a39862e78
Aman Tripathi <imamantripathi@gmail.com>
2023-05-13 17:54:48 +0530

    Why choose us page

--- register.php
+++ register.php
@@ -1,10 +1,14 @@
 <?php
 if(isset($_POST['submit'])) {
     //Get form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+   $name = $_POST["name"];
+   $email = $_POST["email"];
+   $company = $_POST["company"];
+   $phone = $_POST["phone"];
+   $product = $_POST["product"];
+   $website = $_POST["website"];
+   $gstin = $_POST["gstin"];
+   $location = $_POST["location"];
     
     //Recipient email
     $toEmail = 'beyondjustwork@gmail.com';
@@ -23,8 +27,12 @@ if(isset($_POST['submit'])) {
             <p><b>Name:</b> $name</p>
             <p><b>Email:</b> $email</p>
             <p><b>Phone:</b> $phone</p>
-            <p><b>Message:</b></p>
-            <p>$message</p>";
+            <p><b>Company:</b> $company</p>
+            <p><b>Product:</b> $product</p>
+            <p><b>Website:</b> $website</p>
+            <p><b>GSTIN:</b> $gstin</p>
+            <p><b>Location:</b> $location</p>";
+            
     
     //Send email
     if(mail($toEmail, $subject, $body, $headers)) {
